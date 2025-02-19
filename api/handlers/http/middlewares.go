19a952c0f502f3b508a0ac92f264d2e8a2d7fa0b
momoein <momoein711@gmail.com>
2025-02-19 17:16:47 +0330

    feat: add auth middleware

--- api/handlers/http/middlewares.go
+++ api/handlers/http/middlewares.go
@@ -1,19 +1,22 @@
 package http
 
 import (
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/signshine/give-a-sign/pkg/context"
+	appJWT "github.com/signshine/give-a-sign/pkg/jwt"
 	"github.com/signshine/give-a-sign/pkg/logger"
 	"gorm.io/gorm"
 
+	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 )
 
-func SetUserContext(c *fiber.Ctx) error {
+func setUserContext(c *fiber.Ctx) error {
 	c.SetUserContext(context.NewAppContext(c.UserContext(), context.WithLogger(logger.NewLogger())))
 	return c.Next()
 }
 
-func SetTransaction(db *gorm.DB) fiber.Handler {
+func setTransaction(db *gorm.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		tx := db.Begin()
 
@@ -28,8 +31,41 @@ func SetTransaction(db *gorm.DB) fiber.Handler {
 		if err := context.CommitOrRollback(c.UserContext(), true); err != nil {
 			return err
 		}
-		
+
 		return err
 	}
 
+}
+
+func newAuthMiddleware(secret []byte) fiber.Handler {
+	return jwtware.New(jwtware.Config{
+		SigningKey:  jwtware.SigningKey{Key: secret},
+		Claims:      &appJWT.UserClaims{},
+		TokenLookup: "header:Authorization",
+		AuthScheme:  "Bearer",
+		SuccessHandler: func(c *fiber.Ctx) error {
+			claims := userClaims(c)
+			if claims == nil {
+				return fiber.ErrUnauthorized
+			}
+
+			logger := context.GetLogger(c.UserContext())
+			context.SetLogger(c.UserContext(), logger.With("user_id", claims.UserID))
+
+			return c.Next()
+		},
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			return fiber.NewError(fiber.StatusUnauthorized, err.Error())
+		},
+	})
+}
+
+func userClaims(ctx *fiber.Ctx) *appJWT.UserClaims {
+	if u := ctx.Locals("user"); u != nil {
+		userClaims, ok := u.(*jwt.Token).Claims.(*appJWT.UserClaims)
+		if ok {
+			return userClaims
+		}
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/signshine/give-a-sign
 go 1.23.1
 
 require (
+	github.com/gofiber/contrib/jwt v1.0.10
 	github.com/gofiber/fiber/v2 v2.52.6
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
@@ -14,6 +15,7 @@ require (
 )
 
 require (
+	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
